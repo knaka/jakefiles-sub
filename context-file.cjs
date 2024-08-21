d9f5d2e96485d83c0fd9034c02329128448f0df8
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2024-08-21 10:39:55 +0900

    Add context util

---
+++ context-file.cjs
@@ -0,0 +1,29 @@
+const path = require("path");
+const fs = require("fs");
+const { memoize } = require("lodash");
+
+const getConfJson = memoize((filePath) => JSON.parse(fs.readFileSync(filePath)));
+
+const getProjectName = module.exports.getProjectName = (filePath) => getConfJson(filePath)["context"]["project-name"];
+
+const getContextValue = module.exports.getContextValue = (filePath, key, accountNames) => {
+  let value = getConfJson(filePath)["context"][key];
+  for (const accountName of accountNames) {
+    if (
+      getConfJson(filePath)["context"]["overrides"][accountName] && 
+      getConfJson(filePath)["context"]["overrides"][accountName][getEnvName()] &&
+      getConfJson(filePath)["context"]["overrides"][accountName][getEnvName()][key] !== undefined
+    ) {
+      value = getConfJson(filePath)["context"]["overrides"][accountName][getEnvName()][key];
+    }
+  }
+  return value
+}
+
+const getLogEnvName = module.exports.getLongEnvName = () => {
+  return process.env["NODE_ENV"] || process.env["APP_ENV"] || undefined;
+}
+
+const getShortEnvName = module.exports.getShortEnvName = () => {
+  return process.env["NODE_SENV"] || process.env["APP_SENV"] || undefined;
+}
