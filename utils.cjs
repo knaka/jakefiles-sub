7c31c598384098fa6bced82935f2a23dbcb5206a
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2024-08-25 00:04:01 +0900

    Refactor

--- utils.cjs
+++ utils.cjs
@@ -9,3 +9,35 @@ const cleanup = function (fn) {
 module.exports = {
   cleanup,
 }
+/**
+ * @param {string[]} args
+ * @param {string | URL | undefined} cwd
+ * @param {NodeJS.ProcessEnv | undefined} env
+ */
+
+async function asyncRun(args, cwd, env, opts) {
+  const spawnOpts = { cwd, env };
+  if (!opts.input && opts.input !== "") {
+    spawnOpts.stdio = "inherit";
+  }
+  return await new Promise(resolve => {
+    const child = require("child_process").spawn(args[0], args.slice(1), spawnOpts);
+    let output = "";
+    if (child.stdout) {
+      child.stdout.on("data", data => { output += data.toString(); });
+    }
+    child.on("close", (code) => {
+      if (code !== 0) {
+        throw new Error(`Command failed with code ${code}`);
+      }
+      resolve(output);
+    });
+    if (opts.input) {
+      child.stdin.write(opts.input);
+    }
+    if (child.stdin) {
+      child.stdin.end();
+    }
+  });
+}
+exports.asyncRun = asyncRun;
